71c028984f9b9b1db0f466f443452ad56155143f
CharuniPremarathne <kpremarathne996@gmail.com>
2023-12-10 08:18:01 +0530

    fix errors

---
+++ Q1/src/main/java/org/example/Exercise1a.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Exercise1a {
+    public static void main(String[] args) {
+        int x = 1;
+
+        while (x < 10){
+            if(x < 3){
+                System.out.println("big x");
+            }
+
+            /*
+             * increment x
+             *       leads to terminate the program*/
+            x++;
+        }
+    }
+}
